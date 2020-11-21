49c27d43f89325a18a3bb0e1032f51df69e5b752
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2020-11-21 01:52:12 +0100

    edits for final program

--- BIO/bioProject.py
+++ BIO/bioProject.py
@@ -5,27 +5,27 @@ import processWavelet
 import processSobel
 import processHarris
 import processOrientation
-import processLBPLoop
+import processMarkFile
+import processMarkFolder
 
 
 if (sys.argv[1] == "orb"):
     file = sys.argv[2]
     processORB.getORBfeatures(file)
+elif (sys.argv[1] == "markfile"):
+    file = sys.argv[2]
+    processMarkFile.getMarkFile(file)
 elif (sys.argv[1] == "lbp"):
     file = sys.argv[2]
-    right_angle_pixels = processOrientation.getOrientationFeatures(file)
-    processLBP.getLBPfeatures(file, right_angle_pixels)
+    processLBP.getLBPfeatures(file)
 elif (sys.argv[1] == "wavelet"):
     file = sys.argv[2]
     processWavelet.getWaveletFeatures(file)
 elif (sys.argv[1] == "sobel"):
     file = sys.argv[2]
     processSobel.getSobelFeatures(file)
-elif (sys.argv[1] == "harris"):
-    file = sys.argv[2]
-    processHarris.getHarrisFeatures(file)
 elif (sys.argv[1] == "orient"):
     file = sys.argv[2]
     processOrientation.getOrientationFeatures(file)
 elif (sys.argv[1] == "mark"):
-    processLBPLoop.getLBPfeatures()
+    processMarkFolder.getMarkFolder()
--- BIO/processLBP.py
+++ BIO/processLBP.py
@@ -57,88 +57,8 @@ def processLBP(img, x, y, lbp_values):
     lbp_values.append(value_dec) # append new decimal value of pixel to array of whole processed lbp image
     return value_dec
 
-def findWhiteRegions(height, width, lbp_image):
-    print(lbp_image.shape)
-    for i in range(0, height):
-        for j in range(0, width):
-            #print(lbp_image[i][j])
-            gray_level_value = lbp_image[i][j][0]
-            #print(gray_level_value)
-            if (gray_level_value == 255):
-                lbp_image[i, j] = [220,20,60]
 
-    return lbp_image
-
-def computeAverageColorForBlock(lbp_image):
-    rows = np.size(lbp_image, 0)
-    cols = np.size(lbp_image, 1)
-
-    block_div = 7
-    step = 14
-    white_block_pixels = [[]]
-
-    for i in range(block_div, rows-block_div, step):
-        for j in range(block_div, cols-block_div, step):
-            sum_gray_level_pixels = 0.0
-            pixel_count = 0
-            for u in range(i-block_div, i+block_div):
-                for v in range(j-block_div, j+block_div):
-                    gray_level_value = lbp_image[u][v][0]
-                    pixel_count += 1
-                    sum_gray_level_pixels += gray_level_value
-
-            average_block_color = round(sum_gray_level_pixels / pixel_count)
-            #print(average_block_color)
-
-            for u in range(i-block_div, i+block_div):
-                for v in range(j-block_div, j+block_div):
-                    if (average_block_color == 255):
-                        np.append(white_block_pixels, [u,v])
-                    lbp_image[u, v] = [average_block_color,average_block_color,average_block_color]
-
-
-    for i in range(0, rows):
-        for j in range(0, cols):
-            gray_level_value = lbp_image[i][j][0]
-            print(gray_level_value)
-            if (238 <= gray_level_value <= 255):
-                white_block_pixels.append([i,j])
-
-    #print(white_block_pixels)
-
-
-    return white_block_pixels
-
-def markWhiteBlocks(img, white_block_pixels, right_angle_pixels):
-    print(white_block_pixels)
-    rows = np.size(img, 0)
-    cols = np.size(img, 1)
-
-    # filter pixels which are only in white_block_pixels and not right_angle_pixels
-    marked_pixels = []
-    print("White block pixels length:" + str(len(white_block_pixels)))
-    '''
-    for i in range(len(white_block_pixels)):
-        #print(white_block_pixels[i])
-        if white_block_pixels[i] not in right_angle_pixels:
-            marked_pixels.append(white_block_pixels[i])
-    #print(marked_pixels)
-    print("Marked pixels length:" + str(len(marked_pixels)))
-    '''
-
-
-    for i in range(0, rows):
-        for j in range(0, cols):
-            pixel_coordinate_list = []
-            pixel_coordinate_list.append(i)
-            pixel_coordinate_list.append(j)
-            print(pixel_coordinate_list)
-
-            if pixel_coordinate_list in white_block_pixels:
-                img[i, j] = [220,20,60]
-    return img
-
-def getLBPfeatures(file, right_angle_pixels):
+def getLBPfeatures(file):
     img = cv2.imread(file,0)
     img = cv2.normalize(img,None,0,255,cv2.NORM_MINMAX)
     img = processSegmentation.imgSegmentation(img)
@@ -164,16 +84,10 @@ def getLBPfeatures(file, right_angle_pixels):
 
     hist_lbp = cv2.calcHist([lbp_image], [0], None, [256], [0, 256])
 
-    #lbp_image = findWhiteRegions(height, width, lbp_image)
-    white_block_pixels = computeAverageColorForBlock(lbp_image)
-    #print(img.shape)
-    #print(lbp_image.shape)
-    marked_img = markWhiteBlocks(img, white_block_pixels, right_angle_pixels)
-
     # show LBP image and LBP histogram
     figure = plt.figure(figsize=(30, 30))
     image_plot = figure.add_subplot(1,2,1)
-    image_plot.imshow(marked_img)
+    image_plot.imshow(lbp_image)
     image_plot.set_xticks([])
     image_plot.set_yticks([])
     image_plot.set_title("LBP image", fontsize=10)
